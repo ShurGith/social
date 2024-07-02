dd043e254d7d2cf4fbe45c059428f496fb6200fe
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-07-02 22:57:58 +0200

    Instalado Idiomas
    Instalados los dos idiomas

--- config/devdojo/auth/appearance.php
+++ config/devdojo/auth/appearance.php
@@ -12,7 +12,7 @@ return [
         'height' => '32',
     ],
     'background' => [
-        'color' => '#ffffff',
+        'color' => '#c4e3d0',
         'image' => '',
         'image_overlay_color' => '#000000',
         'image_overlay_opacity' => '0.5',
