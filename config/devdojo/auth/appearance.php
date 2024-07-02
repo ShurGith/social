e53478fd3dbc959c081b88bbe967c61b92502811
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-07-02 22:43:03 +0200

    Inicio
    Iniciamos

---
+++ config/devdojo/auth/appearance.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Branding configs for your application
+ */
+
+return [
+    'logo' => [
+        'type' => 'image',
+        'image_src' => '',
+        'svg_string' => '<svg viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><rect width="24" height="24" /></svg>',
+        'height' => '32',
+    ],
+    'background' => [
+        'color' => '#ffffff',
+        'image' => '',
+        'image_overlay_color' => '#000000',
+        'image_overlay_opacity' => '0.5',
+    ],
+    'color' => [
+        'text' => '#212936',
+        'button' => '#09090b',
+        'button_text' => '#ffffff',
+        'input_text' => '',
+        'input_border' => '#212936',
+    ],
+    'alignment' => [
+        'heading' => 'center',
+        'container' => 'center',
+    ],
+    'favicon' => [
+        'light' => '/auth/img/favicon.png',
+        'dark' => '/auth/img/favicon-dark.png',
+    ],
+];
